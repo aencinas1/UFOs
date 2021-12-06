f5f7bedafd888b9aaf4495f0f1dc85877ba6d296
Andre Encinas <andres.encinas@du.edu>
2021-12-05 17:10:28 -0700

    commit module work

--- static/js/app.js
+++ static/js/app.js
@@ -3,3 +3,23 @@ const tableData = data;
 
 //Reference the html toable using d3
 var tbody = d3.select("tbody");
+
+function buildTable(data) {
+    //clear out any existing data
+    tbody.html("");
+
+    //next, loop through each object in the data
+    //and append a row and cells for each value in the row
+    //forEach function
+    data.forEach((dataRow) => {
+        //append a row to the table body
+        let row = tbody.append("tr");
+        
+        //loop through each field in the datarow and add each value as a table cell (td)
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+            cell.text(val);
+            }
+        );
+    });
+}
